6f78d1ad8f8daf5909381ba772ca1e0e77c35a7a
Tamás <1234567zzz@gmail.com>
2017-10-05 19:27:30 +0200

    Changed String to XML

--- Logo/Blockly/Blockly/MainWindow.xaml.cs
+++ Logo/Blockly/Blockly/MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.IO;
 using System.Reflection;
 using System.Windows;
+using System.Xml.Linq;
 
 namespace Blockly
 {
@@ -15,7 +16,7 @@ namespace Blockly
         public MainWindow()
         {
             InitializeComponent();
-            SetBrowserEmulationMode();
+            SetBrowserEmulationMode(); // Changing webbrowser to IE11
             string path = Assembly.GetExecutingAssembly().Location;
             string newPath = Path.GetFullPath(Path.Combine(path, @"..\..\..\..\..\Blockly_Offline\blockly\demos\logo\index.html"));
             browser.Navigate(newPath);
@@ -51,9 +52,20 @@ namespace Blockly
 
         private void injectButton_Click(object sender, RoutedEventArgs e)
         {
+
+            XDocument doc = new XDocument();
+            doc.Add(new XElement("block", new XAttribute("type", "pen_up")));
+
+
+            string xmlString = doc.ToString();
+
+            var script = "var xml = Blockly.Xml.textToDom('<xml>";
+            script += xmlString;
+            script += "</xml>'); Blockly.Xml.domToWorkspace(xml, workspace);";
+
             browser.InvokeScript("clearWorkspace");
 
-            var script = "var xml = Blockly.Xml.textToDom('<xml><block type=\"repeat\"  x=\"10\" y=\"10\"><field name=\"repeat_number\">4</field><statement name=\"repeat\"><block type=\"move_forward\"><value name=\"forward_pixels\"><block type=\"math_number\"><field name=\"NUM\">90</field></block></value><next><block type=\"turn_right\"><value name=\"turn_right\"><block type= \"math_number\"><field name=\"NUM\">90</field></block></value></block></next></block></statement></block></xml>'); Blockly.Xml.domToWorkspace(xml, workspace);";
+            //var script = "var xml = Blockly.Xml.textToDom('<xml><block type=\"repeat\"  x=\"10\" y=\"10\"><field name=\"repeat_number\">4</field><statement name=\"repeat\"><block type=\"move_forward\"><value name=\"forward_pixels\"><block type=\"math_number\"><field name=\"NUM\">90</field></block></value><next><block type=\"turn_right\"><value name=\"turn_right\"><block type= \"math_number\"><field name=\"NUM\">90</field></block></value></block></next></block></statement></block></xml>'); Blockly.Xml.domToWorkspace(xml, workspace);";
 
             browser.InvokeScript("execScript", new Object[] { script, "JavaScript" });
         }
